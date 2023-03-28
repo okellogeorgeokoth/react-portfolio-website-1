a3962d9b3d7784f9d1572057eb2ce7f8d3fc2468
George Okello <okoth603@gmail.com>
2023-03-28 23:03:08 +0300

    Portfolio Project pages

---
+++ src/components/testimonials/Testimonials.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './testimonials.css'
+const testimonials = () => {
+  return (
+    <div>
+      testimonials
+    </div>
+  );
+}
+
+export default testimonials;
