f5376ed9ea4e54df29c68982fc6b09a118fe6709
George Okello <okoth603@gmail.com>
2023-03-29 00:29:23 +0300

    Header commit

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,9 +2,13 @@ import React from 'react';
 import './header.css'
 const header = () => {
   return (
-    <div>
-      header
-    </div>
+<header>
+<div className="container header_container">
+  <h5>Hello I'm</h5>
+  <h1>George okello</h1>
+<h5 className="text-light">Fullstack Developer</h5>
+</div>
+</header>
   );
 }
 
